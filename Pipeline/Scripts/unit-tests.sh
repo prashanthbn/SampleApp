0ddd7b80ef2126035ed663671e32f5050205d129
Prashanth <p25250@gmail.com>
2022-11-02 02:12:59 +0530

    added tests

---
+++ Pipeline/Scripts/unit-tests.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+PARENT_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")";pwd -P)
+echo "PARENT_PATH:" ${PARENT_PATH}
+source ${PARENT_PATH}/header.sh
+
+WD=$(pwd)
+echo "WD:" ${WD}
+
+cd built
+
+###############################################################################
+
+header 'UNIT TESTS STARTED'
+
+dotnet test
+
+header 'UNIT TESTS COMPLETED'
+
+###############################################################################
+
+header 'DELETING UNIT TESTS'
+
+#cd ..
+
+#rm --recursive --force Unit
+ 
+header 'UNIT TESTS DELETED'
+
+###############################################################################
